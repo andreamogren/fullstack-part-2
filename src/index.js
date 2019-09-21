5ae32e0bdc2f57b4e065746c4b9a961ce4ef33a0
Andrea Mogren <andrea.mogren91@gmail.com>
2019-09-21 20:52:27 +0200

    started with part 2

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { log } from 'util';
+
+const notes = [
+    {
+      id: 1,
+      content: 'HTML is easy',
+      date: '2019-05-30T17:30:31.098Z',
+      important: true
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only Javascript',
+      date: '2019-05-30T18:39:34.091Z',
+      important: false
+    },
+    {
+      id: 3,
+      content: 'GET and POST are the most important methods of HTTP protocol',
+      date: '2019-05-30T19:20:14.298Z',
+      important: true
+    }
+  ]
+  
+
+  const App = (props) => {
+    const { notes } = props
+
+    const rows = () => 
+        notes.map(note => <li key={note.id}>{note.content}</li>)
+  
+    return (
+      <div>
+        <h1>Notes</h1>
+        <ul>
+            {rows()}
+        </ul>
+      </div>
+    )
+  }
+
+  const result = notes.map(note => {note.content} )
+  console.log(result);
+  
+
+ReactDOM.render(<App notes={notes}/>, document.getElementById('root'));
