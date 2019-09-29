7155869fbfdad032ba2b52f1f03aae6100e5e6ec
Andrea Mogren <andrea.mogren91@gmail.com>
2019-09-29 12:21:10 +0200

    notes part 2 wip

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react'
-import Course from './components/Course'
+import React, {useState} from 'react'
+import Note from './components/Note'
 
-const App = ({courses}) => {
-  const renderCourses = () => courses.map(course => {
-      return(
-          <Course key={course.id} course={course}/>
-      )
-  })
-  return (
-    <>
-      {renderCourses()}
-    </>
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  //Remove this later
+  console.log(notes)
+
+  const rows = () => notes.map(note => 
+    <Note key={note.id} note={note}/>
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote, 
+      date: new Date().toISOString(), 
+      important: Math.random() > 0.5, 
+      id: notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  return(
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {rows()}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">Save</button>
+      </form>
+    </div>
   )
 }
 
   export default App
-
-  //testing testing
--- src/index.js
+++ src/index.js
@@ -2,49 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-ReactDOM.render(<App courses={courses}/>, document.getElementById('root'));
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+ReactDOM.render(<App notes={notes}/>, document.getElementById('root'));
