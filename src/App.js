fe4d7661639e170f40b01bc9095e43e1e054837b
Andrea Mogren <andrea.mogren91@gmail.com>
2019-10-01 20:26:21 +0200

    2.7 done, 2.8 wip

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
 import React, { useState } from 'react'
 
-const Number = (props) => {
+const Person = (props) => {
   return(
     <>
-    <p>{props.name}</p>
+    <p>{props.name} {props.number}</p>
     </>
   )
 }
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const addPerson= (event) => {
-    console.log(newName)
+  const addPerson = (event) => {
     event.preventDefault()
     const createPerson = () => {
       const personObject = {
         name: newName,
+        number: newNumber,
       }
       setPersons(persons.concat(personObject))
     }
-    console.log(typeof newName)
-    persons.filter(newName => (
-      newName ? alert(`${newName} is already in the phonebook`) : createPerson()  
-    ))
-    setNewName('')
+    const upperCaseName = newName.toUpperCase()
+
+    persons.map(person => {
+      const upperCasePerson = person.name.toUpperCase()
+      if(upperCaseName === upperCasePerson) {
+        setNewName('')
+        setNewNumber('')
+        return alert(`${newName} is already in the phonebook`)
+      } else {
+        setNewName('')
+        setNewNumber('')
+        return createPerson()
+      }
+    })
   }
 
   const renderPersons = () => persons.map(person =>
-    <Number key={person.name} name={person.name}/>
+    <Person key={person.name} name={person.name} number={person.number}/>
   )
 
   return (
     <div>
+      <p>Filter entries:</p> <input onChange={(event) => filterEntries(event.target.value)}/>
       <h2>Phonebook</h2>
       <form>
         <div>
           name: <input onChange={(event) => setNewName(event.target.value)}/>
+          <br/>
+          phone: <input onChange={(event) => setNewNumber(event.target.value)}/>
         </div>
         <div>
           <button type="submit" onClick={addPerson}>add</button>
