a117071979d80a33e36a3bee345690e00e268fe7
Andrea Mogren <mogrenandrea@gmail.com>
2019-10-18 21:24:19 +0200

    continuing with optional exercises in part 2c

--- src/index.js
+++ src/index.js
@@ -2,12 +2,4 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
 
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.render(
-    <App notes={notes} />,
-    document.getElementById('root')
-  )
-})
+ReactDOM.render(<App />, document.getElementById('root'))
