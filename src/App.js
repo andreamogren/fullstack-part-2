0a932c6f0f2792e9f74a7b974d17e602b1e36032
Andrea Mogren <mogrenandrea@gmail.com>
2019-10-19 15:18:35 +0200

    exercise 2.11

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 import ContactForm from './components/ContactForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  const [filteredPersons, setFilteredPersons] = useState([...persons])
+  const [ persons, setPersons ] = useState([]) 
+  const [ filteredPersons, setFilteredPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+
+  useEffect(() => {    
+    console.log('effect')    
+    axios      
+      .get('http://localhost:3001/persons')      
+      .then(response => {        
+        console.log('promise fulfilled')        
+        setPersons(response.data)     
+        setFilteredPersons(response.data) 
+      })  
+    }, [])  
+    console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
