75a24d340b78d0cffc7bd609d2c9c5f3804ec5ca
Andrea Mogren <mogrenandrea@gmail.com>
2019-11-03 20:34:44 +0100

    connecting front end to backend on heroku

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'https://polar-eyrie-05711.herokuapp.com/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
